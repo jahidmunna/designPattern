52fd4f33a2cbd8c01e6221026311dcde82b31b2f
jahidmunna <jimunna150@gmail.com>
2023-10-04 21:08:46 +0900

    factory method patter has been added

---
+++ factorymethod.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class Document(ABC):
+    @abstractmethod
+    def create(self):
+        pass
+
+class PDFDocument(Document):
+    def create(self):
+        return "PDF Document created"
+
+class HTMLDocument(Document):
+    def create(self):
+        return "HTML Document created"
+
+def create_document(doc_type):
+    if doc_type == "pdf":
+        return PDFDocument()
+    elif doc_type == "html":
+        return HTMLDocument()
+    else:
+        raise ValueError("Invalid document type")
+
+pdf_doc = create_document("pdf")
+print(pdf_doc.create())  # Output: PDF Document created
